5d37e7e15517a7442796aad6eefff2e71e4653f9
Benjamin Buhler <2802675+bbuhler@users.noreply.github.com>
2026-04-16 12:22:12 +0200

    fix: set current environment in short description

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,16 +3,19 @@ import { bot } from '$lib/server/bot';
 import { GrammyError } from 'grammy';
 
 if (dev) {
-	bot.start().catch((err) => {
-		if (
-			!(
-				err instanceof GrammyError &&
-				(err.error_code === 409 || err.description.includes('query is too old'))
-			)
-		) {
-			throw err;
-		}
-	}).then(() => {
-		void bot.api.setMyShortDescription('development');
-	});
+	bot
+		.start()
+		.then(() => {
+			void bot.api.setMyShortDescription('development');
+		})
+		.catch((err) => {
+			if (
+				!(
+					err instanceof GrammyError &&
+					(err.error_code === 409 || err.description.includes('query is too old'))
+				)
+			) {
+				throw err;
+			}
+		});
 }
